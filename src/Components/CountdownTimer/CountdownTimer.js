e976117c96ff08f80d7d0a4a43665a61832873be
adrian.clivet <adrian.clivet@agilenetworks.tech>
2021-09-10 18:03:17 +0300

    feat: grab input values and transform to ms

--- src/Components/CountdownTimer/CountdownTimer.js
+++ src/Components/CountdownTimer/CountdownTimer.js
@@ -1,45 +1,94 @@
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import TimerIcon from "@material-ui/icons/Timer";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CountdownTimer.scss";
 
+const daysToMs = 1000 * 60 * 60 * 24;
+const hoursToMs = 1000 * 60 * 60;
+const minutesToMs = 1000 * 60;
+
 export const CountdownTimer = () => {
+  const [eventName, setEventName] = useState("");
+  const [eventDate, setEventDate] = useState("");
+  const [eventTime, setEventTime] = useState("");
+  const [startCountdown, setStartCountdown] = useState(false);
+  const [newEventDate, setNewEventDate] = useState(null);
+  const [presentTime, setPresentTime] = useState(null);
+  const [remainingTime, setRemainingTime] = useState(null);
+
+  useEffect(() => {
+    if (startCountdown) {
+      setPresentTime(new Date().getTime());
+      setNewEventDate(new Date(`${eventDate + eventTime}`).getTime());
+      setRemainingTime(Number(newEventDate) - Number(presentTime));
+    }
+    console.log("newEvent:", newEventDate, typeof newEventDate);
+    console.log("presentTime:", presentTime);
+    console.log("remainingTime:", remainingTime);
+
+    return () => {
+      setStartCountdown(false);
+    };
+  }, [
+    eventDate,
+    eventTime,
+    newEventDate,
+    presentTime,
+    remainingTime,
+    startCountdown,
+  ]);
+
   return (
     <div className="countdown">
       <div className="countdown__title">
         <TimerIcon className="item__icon" />
         <p>Countdown Timer</p>
       </div>
-
-      <form className="countdown__inputs">
-        <TextField
-          className="input-field"
-          id="filled-basic"
-          label="Event name"
-          variant="filled"
-        />
-        <TextField
-          className="input-field"
-          id="date"
-          label="Your event's date"
-          type="date"
-          variant="filled"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          className="input-field"
-          id="time"
-          label="Your event's time"
-          type="time"
-          defaultValue="00:00"
-          variant="filled"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </form>
+      <div className="test">
+        <form className="countdown__inputs">
+          <TextField
+            className="input-field"
+            id="filled-basic"
+            label="Event name"
+            variant="filled"
+            onChange={(e) => setEventName(e.target.value)}
+          />
+          <TextField
+            className="input-field"
+            id="date"
+            label="Your event's date"
+            type="date"
+            variant="filled"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setEventDate(e.target.value)}
+          />
+          <TextField
+            className="input-field"
+            id="time"
+            label="Your event's time *optional*"
+            type="time"
+            defaultValue="00:00"
+            variant="filled"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) =>
+              e.target.value ? setEventTime(`T${e.target.value}:00`) : null
+            }
+          />
+        </form>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() =>
+            eventName && eventDate ? setStartCountdown(true) : null
+          }
+        >
+          Start
+        </Button>
+      </div>
     </div>
   );
 };
---
+++ src/Components/CountdownTimer/timeUnits.js
@@ -0,0 +1,5 @@
+export const timeUnits = () => {
+  const daysToMs = 1000 * 60 * 60 * 24;
+  const hoursToMs = 1000 * 60 * 60;
+  const minutesToMs = 1000 * 60;
+};
